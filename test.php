ae49cbffebe69977fd83ea132965592326c9333f
drozas <david.rozas@gmail.com>
2015-06-16 17:33:44 +0100

    Adding minimal instructions

--- test.php
+++ test.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * - Clone repo: https://github.com/drozas/drupal-org-api
+ * - Install composer: https://getcomposer.org/doc/00-intro.md
+ * 		curl -sS https://getcomposer.org/installer | php
+ * 		mv composer.phar /usr/local/bin/composer
+ * 		~/workspace/drupal-org-api$ composer install
+ * - Add vendor autoload
+ */
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
