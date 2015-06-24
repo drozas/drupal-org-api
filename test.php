c07ef5d25f41e1be4df590c8d995fae484a4d02d
David Rozas <david.rozas@gmail.com>
2015-06-24 13:02:10 +0200

    Adding some more nasty code to fetch mentorship relationships

--- test.php
+++ test.php
@@ -11,20 +11,53 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
+const LAST_UID = 3248108;
+const DROZAS_UID = 740628;
+
+$db_hostname = "localhost";
+$db_username = "dorg_mentors";
+$db_password = "dorg_mentors";
+$db_name = "dorg_mentors";
+
 require 'vendor/autoload.php';
 use EclipseGc\DrupalOrg\Api\DrupalClient;
 
-$client = DrupalClient::create();
+
+try
+{
+	// Create connection
+	$conn = new mysqli($db_hostname, $db_username, $db_password, $db_name);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$client = DrupalClient::create();
+
+	for ($i = DROZAS_UID; $i <= DROZAS_UID + 10; $i++) {
+		//Get user 
+		$user = $client->getUser($i);
+		
+		//Keep mentored in memory. More efficient?
+		$mentored_uid = $user->getUid();
+		$mentored_username = $user->getName();
+		
+		//Add a new tuple for each mentored_by
+		foreach ($user->getMentors() as $mentored_by){
+			$mentored_by_uid = $mentored_by->getUid();
+			$mentored_by_username = $mentored_by->getName();
+			$sql = "INSERT INTO mentored_by (mentored_uid, mentored_by_uid, mentored_username, mentored_by_username)
+			VALUES ($mentored_uid, $mentored_by_uid, '$mentored_username', '$mentored_by_username')";
+			$result = $conn->query($sql);
+		}
+	}
 
 
-$user = $client->getUser(348120);
-echo "The mentors of " . $user->getName() . "(" . $user->getFirstName() . " " . $user->getlastName() .  ") are:";
-foreach ($user->getMentors() as $mentor){
- echo "<br \>" . $mentor->getName() . "(" . $mentor->getFirstName() . " " . $mentor->getlastName() .  ")";
 
- }
-/*foreach ($user->getAttendance() as $attendance){
-	echo $event;
-}*/
+	$conn->close();
+} catch (Exception $e)
+{
+	echo 'Caught exception: ', $e->getMessage(), "\n";
+}
 
 ?>
