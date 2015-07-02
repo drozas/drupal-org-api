420210faa3ebe06a4bbe3ed37a98ce743a0f52d9
David Rozas <david.rozas@gmail.com>
2015-07-02 12:01:30 +0300

    Running execution up to 207648

--- fetch_dorg_mentorships_cli.php
+++ fetch_dorg_mentorships_cli.php
@@ -37,6 +37,8 @@ const RUN_7 = 26393;
 const RUN_8 = 179766;
 const RUN_9 = 194223;
 const RUN_10 = 207009;
+const RUN_11 = 207648;
+
 
 
 
@@ -63,7 +65,7 @@ try
 	//Instantiate SDK client
 	$client = DrupalClient::create();
 
-	for ($i = RUN_10; $i <= LAST_UID; $i++) {
+	for ($i = RUN_11; $i <= LAST_UID; $i++) {
 		// Fetch whole user object. Catch possible error responses from API (e.g. 403)
 		try {
 			$user = $client->getUser($i);
